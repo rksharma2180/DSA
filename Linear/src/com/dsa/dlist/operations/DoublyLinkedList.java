fa44939a071be28e4888f207fa30929a249a1ade
ravi <rksharma2180@gmail.com>
2023-09-15 08:40:06 +0530

    Doubly Linked List

--- Linear/src/com/dsa/dlist/operations/DoublyLinkedList.java
+++ Linear/src/com/dsa/dlist/operations/DoublyLinkedList.java
@@ -3,20 +3,19 @@ package com.dsa.dlist.operations;
 import java.util.Scanner;
 import java.util.function.Consumer;
 
-import com.dsa.list.operations.Node;
-
 public class DoublyLinkedList {
-	
+
 	static Scanner scanner = new Scanner(System.in);
-	static Node<Object> node;
-	static Node<Object> start;
-	static Node<Object> currentNode;
-	static Consumer<Object> print = s -> System.out.println(s);
-	
+	static Node<Integer> head;
+	static Node<Integer> currentNode;
+	static Consumer<Object> print = s -> System.out.print(s);
+
 	public static void main(String[] args) {
-		
+		while(true) {
+			showOptions();
+		}
 	}
-	
+
 	public static void showOptions() {
 		int input = 0;
 		print.accept("\n Selection Options \n" + "1: Create List \n" + "2: Add Element \n"
@@ -57,5 +56,91 @@ public class DoublyLinkedList {
 		default:
 			System.exit(0);
 		}
+	}
+
+	private static void reverseList() {
+
+	}
+
+	private static void printReverseList() {
+		Node<Integer> temp = currentNode;
+		do {
+			print.accept(temp.data+"<-->");
+			temp = temp.prev;
+		} while(temp.prev !=null);
+		print.accept(head.data);
+	}
+
+	private static void showElements() {
+		Node<Integer> temp = head;
+		if(temp != null) {
+			while(temp.next != null) {
+				print.accept(temp.data + "<-->");
+				temp = temp.next;
+			}
+			print.accept("" + currentNode.data + "->"+currentNode.next);
+		} else {
+			print.accept("List is empty");
+		}
+	}
+
+	private static void addAtTheEnd() {
+		addElements();
+	}
+
+	private static void addAtBegining() {
+		print.accept("Enter Element");
+		int element = scanner.nextInt();
+		Node<Integer> temp = new Node<>(element, head, null);
+		head = temp;
+	}
+
+	private static void searchElement() {
+		print.accept("Enter Element");
+		int element = scanner.nextInt();
+		Node<Integer> node = findNode(head, element);
+		if (null != node) {
+			print.accept("Element found");
+			return;
+		}
+		print.accept("Element not found");
+	}
+
+	private static void deleteElement() {
+		print.accept("Enter Element");
+		int element = scanner.nextInt();
+		Node<Integer> node = findNode(head, element);
+		if (null != node) {
+			if(node != head)
+				node.prev.next = node.next;
+			else 
+				head = node.next;
+			print.accept("Element Deleted Successfully " + node.data);
+		}
+	}
+
+	private static Node<Integer> findNode(Node<Integer> head, Integer element){
+		Node<Integer> temp = head;
+		do {
+			if (temp.data.equals(element)) {
+				return temp;
+			}
+			temp = temp.next;
+		} while (temp.next != null);
+		return null;
+	}
+
+	private static void addElements() {
+		print.accept("Enter Element");
+		int element = scanner.nextInt();
+		Node<Integer> temp = new Node<>(element, null, currentNode);
+		currentNode.next = temp;
+		currentNode = temp;
+	}
+
+	private static void createList() {
+		print.accept("Enter First Element");
+		int element = scanner.nextInt();
+		head = currentNode = new Node<>(element, null, null);
 	}
 }
